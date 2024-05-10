fb0a8825a95b1e8fcc58c05a9507db588f608e6c
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-05-10 09:41:20 +0530

    clear things up and try to implement first scence

--- starter/main.js
+++ starter/main.js
@@ -1,24 +1,32 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const canvas = document.querySelector('canvas.webgl')
 
-setupCounter(document.querySelector('#counter'))
+//scence
+const scence = new THREE.Scene()
+
+//object
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color:0xff0000})
+const mesh = new THREE.Mesh(geometry,material)
+
+scence.add(mesh)
+
+//sizes
+
+const sizes = {
+  height: 600,
+  width: 800
+}
+
+//camera
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
+
+scence.add(camera)
+
+//renderer
+
+const renderer = new THREE.WebGLRenderer({
+  canvas:canvas
+})
