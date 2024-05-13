9c598227ea8232d8095e258f7825cef8d0ac181a
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-05-13 09:03:33 +0530

    making transformations
    position,rotation,scale,quaternion are main
    rotation.reorder
    object.lookat
    are the other helpers

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -12,7 +12,36 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+
+
+//position
+// mesh.position.x=0.7
+// mesh.position.x=-0.6
+// mesh.position.x=1
+
+mesh.position.set(0.7,-0.6,1)
+
+//scale
+
+// mesh.scale.x = 0.5
+// mesh.scale.x = 1.5
+// mesh.scale.x = 2
+
+mesh.scale.set(2,0.5,0.5)
+
+//rotation
+
+//you can give the order to rotate
+mesh.rotation.reorder('YXZ')
+
+mesh.rotation.x = Math.PI * 0.25
+mesh.rotation.y = Math.PI * 0.25
+
+
 scene.add(mesh)
+
+const axis = new THREE.AxesHelper()
+scene.add(axis)
 
 /**
  * Sizes
@@ -27,6 +56,8 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+
+camera.lookAt(mesh.position)
 scene.add(camera)
 
 /**
