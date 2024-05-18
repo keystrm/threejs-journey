69acd8181230242c76ad501c522f65155a208341
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-05-18 22:02:40 +0530

    rotation using cos and sin functions

--- 06-cameras/src/script.js
+++ 06-cameras/src/script.js
@@ -80,8 +80,10 @@ const tick = () =>
     mesh.rotation.y = elapsedTime;
 
     //update camera
-    camera.position.x = cursor.x
-    camera.position.y = cursor.y
+    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2
+    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2
+    camera.position.y = cursor.y * 3
+    camera.lookAt(mesh.position)
 
     // Render
     renderer.render(scene, camera)
