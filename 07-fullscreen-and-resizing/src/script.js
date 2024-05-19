d6a7bda1308ae18a5b5dd43dca10bd934dcf01a1
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-05-19 13:25:36 +0530

    set pixel ratio

--- 07-fullscreen-and-resizing/src/script.js
+++ 07-fullscreen-and-resizing/src/script.js
@@ -60,6 +60,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
 
 /**
  * Animate
