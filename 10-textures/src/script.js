8a0136cbf8edad16eba34ca41dd7453533a9c71d
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-05-20 08:18:49 +0530

    min Filters

--- 10-textures/src/script.js
+++ 10-textures/src/script.js
@@ -24,9 +24,11 @@ const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 
 // colorTexture.offset.x = 0.5
 
-colorTexture.rotation = Math.PI/4
-colorTexture.center.x = 0.5
-colorTexture.center.y = 0.5
+// colorTexture.rotation = Math.PI/4
+// colorTexture.center.x = 0.5
+// colorTexture.center.y = 0.5
+
+colorTexture.minFilter = THREE.NearestFilter
 
 /**
  * Base
