5d5b153194be7b13e7f7cd51a38881e8d7bade5b
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-05-20 07:49:47 +0530

    loading manager to manage more textures

--- 10-textures/src/script.js
+++ 10-textures/src/script.js
@@ -5,10 +5,17 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 /**
  * Textures
  */
+const loadingManager = new THREE.LoadingManager()
 
-const textureLoader = new THREE.TextureLoader()
-const texture = textureLoader.load('/textures/door/color.jpg')
-texture.colorSpace = THREE.SRGBColorSpace
+const textureLoader = new THREE.TextureLoader(loadingManager)
+const colorTexture = textureLoader.load('/textures/door/color.jpg')
+colorTexture.colorSpace = THREE.SRGBColorSpace
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const heightTexture = textureLoader.load('/textures/door/height.jpg')
+const normalTexture = textureLoader.load('/textures/door/normal.jpg')
+const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 
 /**
  * Base
@@ -23,7 +30,7 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ map:texture })
+const material = new THREE.MeshBasicMaterial({ map:colorTexture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
