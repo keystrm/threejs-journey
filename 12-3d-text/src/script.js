b1788d14c68a445c5548f8b084176e6ea5e8d761
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-05-23 20:18:37 +0530

    load font loader and font

--- 12-3d-text/src/script.js
+++ 12-3d-text/src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import {FontLoader} from 'three/examples/jsm/loaders/FontLoader.js'
 
 /**
  * Base
@@ -18,6 +19,15 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+
+/**
+ * Font Loader
+ */
+const fontLoader = new FontLoader()
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    ()=>{}
+)
 
 /**
  * Object
