f15c6f12d0e678512b53a9a6c4fc462f4603f317
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-05-23 23:18:23 +0530

    directional light to sun like effect

--- 14-lights/src/script.js
+++ 14-lights/src/script.js
@@ -17,14 +17,11 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 1.5)
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
 scene.add(ambientLight)
 
-const pointLight = new THREE.PointLight(0xffffff, 50)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+const directionalLight = new THREE.DirectionalLight(0x00fffc,1)
+scene.add(directionalLight)
 
 /**
  * Objects
