b36121a6047b43625ce77b1566a3523b205d2173
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-05-23 13:43:56 +0530

    add shadows where its too dark using ao maps

--- 11-materials/src/script.js
+++ 11-materials/src/script.js
@@ -81,6 +81,8 @@ const material = new THREE.MeshStandardMaterial()
 material.metalness = 0.7
 material.roughness = 0.2
 material.map = doorColorTexture
+material.aoMap = doorAmbientOcclusionTexture
+material.aoMapIntensity = 1
 
 gui.add(material,'metalness').min(0).max(1).step(0.0001)
 gui.add(material,'roughness').min(0).max(1).step(0.0001)
