07919422bb2f9585fd28dafbc8057eb18fab149f
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-05-23 14:13:19 +0530

    fluffy effect using sheen

--- 11-materials/src/script.js
+++ 11-materials/src/script.js
@@ -115,15 +115,23 @@ material.normalMap = doorNormalTexture
 material.normalScale.set(0.5, 0.5)
 
 // Clearcoat
-material.clearcoat = 1
-material.clearcoatRoughness = 0
+// material.clearcoat = 1
+// material.clearcoatRoughness = 0
 
-gui.add(material, 'clearcoat').min(0).max(1).step(0.0001)
-gui.add(material, 'clearcoatRoughness').min(0).max(1).step(0.0001)
+// gui.add(material, 'clearcoat').min(0).max(1).step(0.0001)
+// gui.add(material, 'clearcoatRoughness').min(0).max(1).step(0.0001)
 
-gui.add(material,'metalness').min(0).max(1).step(0.0001)
-gui.add(material,'roughness').min(0).max(1).step(0.0001)
+// gui.add(material,'metalness').min(0).max(1).step(0.0001)
+// gui.add(material,'roughness').min(0).max(1).step(0.0001)
 
+// Sheen
+material.sheen = 1
+material.sheenRoughness = 0.25
+material.sheenColor.set(1, 1, 1)
+
+gui.add(material, 'sheen').min(0).max(1).step(0.0001)
+gui.add(material, 'sheenRoughness').min(0).max(1).step(0.0001)
+gui.addColor(material, 'sheenColor')
 
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(0.5,64,64),
