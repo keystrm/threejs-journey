6d65d5a7a6f54e8f9b5a95afb337a7e7ecc65554
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-05-23 21:43:54 +0530

    use only one material to rule em all

--- 12-3d-text/src/script.js
+++ 12-3d-text/src/script.js
@@ -63,16 +63,15 @@ fontLoader.load(
         const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
         matcapTexture.colorSpace = THREE.SRGBColorSpace
 
-        const textMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
-        const text = new THREE.Mesh(textGeometry,textMaterial)
+        const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+        const text = new THREE.Mesh(textGeometry,material)
         scene.add(text)
 
         const donutGeometry = new THREE.TorusGeometry(0.3,0.2,20,45)
-        const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
 
-        for(let i=0;i<100;i++){
+        for(let i=0;i<300;i++){
 
-            const donut = new THREE.Mesh(donutGeometry,donutMaterial)
+            const donut = new THREE.Mesh(donutGeometry,material)
 
             donut.position.x = (Math.random()-0.5) * 10
             donut.position.y = (Math.random()-0.5) * 10
