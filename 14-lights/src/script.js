1ac70615c7127ed8d5c6f0c782ba85b13b311152
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-05-23 23:47:42 +0530

    spot light with controls

--- 14-lights/src/script.js
+++ 14-lights/src/script.js
@@ -38,6 +38,13 @@ scene.add(rectAreaLight)
 rectAreaLight.position.set(- 1.5, 0, 1.5)
 rectAreaLight.lookAt(new THREE.Vector3())
 
+// Spot light
+const spotLight = new THREE.SpotLight(0x78ff00, 4.5, 10, Math.PI * 0.1, 0.25, 1)
+spotLight.position.set(0, 2, 3)
+scene.add(spotLight)
+spotLight.target.position.x = - 0.75
+scene.add(spotLight.target)
+
 /**
  * Objects
  */
