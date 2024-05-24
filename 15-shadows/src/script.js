51b4fedc7d992bde61b1aef31d7303df819b2c74
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-05-24 11:44:57 +0530

    add shadows as a mesh

--- 15-shadows/src/script.js
+++ 15-shadows/src/script.js
@@ -96,6 +96,9 @@ const textureLoader = new THREE.TextureLoader()
 const bakedShadow = textureLoader.load('/textures/bakedShadow.jpg')
 bakedShadow.colorSpace = THREE.SRGBColorSpace
 
+const simpleShadow = textureLoader.load('/textures/simpleShadow.jpg')
+simpleShadow.colorSpace = THREE.SRGBColorSpace
+
 /**
  * Materials
  */
@@ -117,7 +120,7 @@ sphere.castShadow = true
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
     new THREE.MeshBasicMaterial({
-        map:bakedShadow
+        material
     })
 )
 plane.rotation.x = - Math.PI * 0.5
@@ -125,6 +128,19 @@ plane.position.y = - 0.5
 plane.receiveShadow = true
 
 scene.add(sphere, plane)
+
+const sphereShadow = new THREE.Mesh(
+    new THREE.PlaneGeometry(1.5, 1.5),
+    new THREE.MeshBasicMaterial({
+        color: 0x000000,
+        transparent: true,
+        alphaMap: simpleShadow
+    })
+)
+sphereShadow.rotation.x = - Math.PI * 0.5
+sphereShadow.position.y = plane.position.y + 0.01
+
+scene.add(sphere, sphereShadow, plane)
 
 /**
  * Sizes
