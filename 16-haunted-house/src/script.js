9864fb00bd33615c6a59ebf5773c87b4a77ebb25
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-05-26 18:13:20 +0530

    hide unwanted thnings

--- 16-haunted-house/src/script.js
+++ 16-haunted-house/src/script.js
@@ -194,18 +194,22 @@ const bushMaterial = new THREE.MeshStandardMaterial({
 const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush1.scale.set(0.5, 0.5, 0.5)
 bush1.position.set(0.8, 0.2, 2.2)
+bush1.rotation.x = - 0.75
 
 const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush2.scale.set(0.25, 0.25, 0.25)
 bush2.position.set(1.4, 0.1, 2.1)
+bush2.rotation.x = - 0.75
 
 const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush3.scale.set(0.4, 0.4, 0.4)
 bush3.position.set(- 0.8, 0.1, 2.2)
+bush3.rotation.x = - 0.75
 
 const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush4.scale.set(0.15, 0.15, 0.15)
 bush4.position.set(- 1, 0.05, 2.6)
+bush4.rotation.x = - 0.75
 
 house.add(bush1, bush2, bush3, bush4)
 
