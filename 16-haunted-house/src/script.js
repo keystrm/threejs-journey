e38b055c630574747f7070430000c510b8dc1527
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-05-26 18:46:27 +0530

    optimize shadows using maps

--- 16-haunted-house/src/script.js
+++ 16-haunted-house/src/script.js
@@ -369,6 +369,27 @@ for(const grave of graves.children)
     grave.receiveShadow = true
 }
 
+// Mappings
+directionalLight.shadow.mapSize.width = 256
+directionalLight.shadow.mapSize.height = 256
+directionalLight.shadow.camera.top = 8
+directionalLight.shadow.camera.right = 8
+directionalLight.shadow.camera.bottom = - 8
+directionalLight.shadow.camera.left = - 8
+directionalLight.shadow.camera.near = 1
+directionalLight.shadow.camera.far = 20
+
+ghost1.shadow.mapSize.width = 256
+ghost1.shadow.mapSize.height = 256
+ghost1.shadow.camera.far = 10
+
+ghost2.shadow.mapSize.width = 256
+ghost2.shadow.mapSize.height = 256
+ghost2.shadow.camera.far = 10
+
+ghost3.shadow.mapSize.width = 256
+ghost3.shadow.mapSize.height = 256
+ghost3.shadow.camera.far = 10
 
 /**
  * Animate
