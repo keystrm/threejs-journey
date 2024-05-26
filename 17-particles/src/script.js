e5e6592a8eec3ff13a84de0471ef09c51cb95636
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-05-26 19:54:40 +0530

    add particles

--- 17-particles/src/script.js
+++ 17-particles/src/script.js
@@ -24,7 +24,17 @@ const textureLoader = new THREE.TextureLoader()
  */
 
 // Geometry
-const particlesGeometry = new THREE.SphereGeometry(1, 32, 32)
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 500
+
+const positions = new Float32Array(count * 3) // Multiply by 3 because each position is composed of 3 values (x, y, z)
+
+for(let i = 0; i < count * 3; i++) // Multiply by 3 for same reason
+{
+    positions[i] = (Math.random() - 0.5) * 10 // Math.random() - 0.5 to have a random value between -0.5 and +0.5
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3)) // Create the Three.js BufferAttribute and specify that each information is composed of 3 values
 
 const particlesMaterial = new THREE.PointsMaterial({
     size:0.02,
