74f39aab8108a0f19ac227a6243971d203310dab
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-05-26 22:00:32 +0530

    add colors

--- 18-galaxy-generator/src/script.js
+++ 18-galaxy-generator/src/script.js
@@ -25,6 +25,8 @@ parameters.branches = 3
 parameters.spin = 1
 parameters.randomness = 0.2
 parameters.randomnessPower = 3
+parameters.insideColor = '#ff6030'
+parameters.outsideColor = '#1b3984'
 
 let geometry = null
 let material = null
@@ -46,6 +48,7 @@ const generateGalaxy = () =>
     geometry = new THREE.BufferGeometry()
 
     const positions = new Float32Array(parameters.count * 3)
+    const colors = new Float32Array(parameters.count * 3)
 
     for(let i = 0; i < parameters.count; i++)
     {
@@ -54,9 +57,14 @@ const generateGalaxy = () =>
         positions[i3    ] = (Math.random() - 0.5) * 3
         positions[i3 + 1] = (Math.random() - 0.5) * 3
         positions[i3 + 2] = (Math.random() - 0.5) * 3
+
+        colors[i3    ] = 1
+        colors[i3 + 1] = 0
+        colors[i3 + 2] = 0
     }
 
     geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
 
     /**
      * Material
@@ -65,7 +73,8 @@ const generateGalaxy = () =>
         size: parameters.size,
         sizeAttenuation: true,
         depthWrite: false,
-        blending: THREE.AdditiveBlending
+        blending: THREE.AdditiveBlending,
+        vertexColors: true
     })
 
      /**
@@ -97,6 +106,8 @@ generateGalaxy()
 
 gui.add(parameters, 'count').min(100).max(1000000).step(100)
 gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001)
+gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
+gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
 gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
 gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
 gui.add(parameters, 'spin').min(- 5).max(5).step(0.001).onFinishChange(generateGalaxy)
