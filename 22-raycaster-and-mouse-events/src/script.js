5a4f637ae810d0ec8b312752d8c33ab4b280a6e8
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-05-29 08:39:45 +0530

    change color with mouse points

--- 22-raycaster-and-mouse-events/src/script.js
+++ 22-raycaster-and-mouse-events/src/script.js
@@ -15,6 +15,19 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Mouse
+ */
+const mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', (event) =>
+{
+    mouse.x = event.clientX / sizes.width * 2 - 1
+    mouse.y = - (event.clientY / sizes.height) * 2 + 1
+
+    console.log(mouse)
+})
+
+/**
  * Objects
  */
 const object1 = new THREE.Mesh(
@@ -106,25 +119,43 @@ const tick = () =>
     object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
 
     // Cast a ray
-    const rayOrigin = new THREE.Vector3(- 3, 0, 0)
-    const rayDirection = new THREE.Vector3(1, 0, 0)
-    rayDirection.normalize()
+    // const rayOrigin = new THREE.Vector3(- 3, 0, 0)
+    // const rayDirection = new THREE.Vector3(1, 0, 0)
+    // rayDirection.normalize()
     
-    raycaster.set(rayOrigin, rayDirection)
+    // raycaster.set(rayOrigin, rayDirection)
+    
+    // const objectsToTest = [object1, object2, object3]
+    // const intersects = raycaster.intersectObjects(objectsToTest)
+
+    // for(const object of objectsToTest)
+    // {
+    //     object.material.color.set('#ff0000')
+    // }
+
+    // for(const intersect of intersects)
+    // {
+    //     intersect.object.material.color.set('#0000ff')
+    // }
+
+    raycaster.setFromCamera(mouse, camera)
     
     const objectsToTest = [object1, object2, object3]
     const intersects = raycaster.intersectObjects(objectsToTest)
-
-    for(const object of objectsToTest)
-    {
-        object.material.color.set('#ff0000')
-    }
-
+    
     for(const intersect of intersects)
     {
         intersect.object.material.color.set('#0000ff')
     }
 
+    for(const object of objectsToTest)
+    {
+        if(!intersects.find(intersect => intersect.object === object))
+        {
+            object.material.color.set('#ff0000')
+        }
+    }
+    
     // Update controls
     controls.update()
 
