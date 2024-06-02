e962abd6f6127db4af78ded481566b1155e6616e
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-02 09:30:37 +0530

    initiate camera

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,5 +1,6 @@
 import Size from "./Utills/Size"
 import Time from './Utills/Time.js'
+import Camera from "./Camera.js"
 import * as THREE from 'three'
 export default class Experience{
     constructor(canvas,sizes){
@@ -9,6 +10,12 @@ export default class Experience{
 
         //size
         this.size = new Size(sizes.width,sizes.height,sizes.ratio)
+        //time
+        this.time = new Time()
+        //scene
+        this.scene = new THREE.Scene()
+        //camera
+        this.camera = new Camera()
 
         // Resize event
         this.size.on('resize', () =>
@@ -16,17 +23,12 @@ export default class Experience{
             console.log('A resize occurred')
         })
 
-        //time
-        this.time = new Time()
-
         // Time tick event
         this.time.on('tick', () =>
         {
             this.update()
         })
 
-        //scene
-        this.scene = new THREE.Scene()
     }
 
     resize(){
