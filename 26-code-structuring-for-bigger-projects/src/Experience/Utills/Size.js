5575c07ff59205625664fec146fb3d0aeb1563c8
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-02 09:14:46 +0530

    initiate event size using event emitter

--- 26-code-structuring-for-bigger-projects/src/Experience/Utills/Size.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Utills/Size.js
@@ -1,7 +1,14 @@
-export default class Size{
+import EventEmitter from './EventEmitter.js'
+export default class Size extends EventEmitter{
     constructor(width,height,ratio){
+        super()
         this.width = width
         this.height = height
         this.ratio = ratio
+
+        window.addEventListener('resize', () =>
+        {
+            this.trigger('resize')
+        })
     }
 }
