57962dcf96ebdc534a8308cd1f5725d984ac63bd
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-03 22:06:26 +0530

    remove sample box

--- 26-code-structuring-for-bigger-projects/src/Experience/World/World.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/World/World.js
@@ -9,13 +9,6 @@ export default class World
     {
         this.experience = new Experience()
         this.scene = this.experience.scene
-        
-        // Test mesh
-        const testMesh = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshStandardMaterial()
-        )
-        this.scene.add(testMesh)
 
         this.resources = this.experience.resources
 
