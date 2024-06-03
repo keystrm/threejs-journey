dba28af1bf59178a02c8f2bafc1d0517f2dd641d
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-03 23:53:27 +0530

    destroying things

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -61,4 +61,36 @@ export default class Experience{
         this.world.update()
         this.renderer.update()
     }
+    destroy()
+    {
+        this.size.off('resize')
+        this.time.off('tick')
+        // Traverse the whole scene
+        // Traverse the whole scene
+        this.scene.traverse((child) =>
+        {
+            // Test if it's a mesh
+            if(child instanceof THREE.Mesh)
+            {
+                child.geometry.dispose()
+
+                // Loop through the material properties
+                for(const key in child.material)
+                {
+                    const value = child.material[key]
+
+                    // Test if there is a dispose function
+                    if(value && typeof value.dispose === 'function')
+                    {
+                        value.dispose()
+                    }
+                }
+            }
+        })
+
+        this.camera.controls.dispose()
+        this.renderer.instance.dispose()
+        if(this.debug.active)
+            this.debug.ui.destroy()
+    }
 }
