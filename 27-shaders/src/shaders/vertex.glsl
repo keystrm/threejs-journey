d43e88f711e4cb0f73d2321834799c33a8f30210
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-06 23:07:33 +0530

    add varying shadows

--- 27-shaders/src/shaders/vertex.glsl
+++ 27-shaders/src/shaders/vertex.glsl
@@ -11,16 +11,23 @@ varying float vRandom;
 
 attribute vec2 uv;
 varying vec2 vUv;
+varying float vElevation;
 
 void main(){
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    modelPosition.z += sin(modelPosition.x * uFrequency.x + uTime) * 0.1;
-    modelPosition.z += sin(modelPosition.y * uFrequency.y + uTime) * 0.1;
+    // modelPosition.z += sin(modelPosition.x * uFrequency.x + uTime) * 0.1;
+    // modelPosition.z += sin(modelPosition.y * uFrequency.y + uTime) * 0.1;
+
+    float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+    elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+    
+    modelPosition.z += elevation;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
     gl_Position = projectionPosition;
 
+    vElevation = elevation;
     vRandom = aRandom;
     vUv = uv;
 }
