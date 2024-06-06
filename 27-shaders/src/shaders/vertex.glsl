43483b123d79658cfb8cbc5a5ab03502b4ec860f
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-06 07:52:25 +0530

    getting flag like effects

--- 27-shaders/src/shaders/vertex.glsl
+++ 27-shaders/src/shaders/vertex.glsl
@@ -5,6 +5,8 @@ attribute vec3 position;
 
 void main(){
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    modelPosition.z += sin(modelPosition.x * 20.0) * 0.1;
+
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
     gl_Position = projectionPosition;
