e06a5cbc43711156da43bf565800edcb6f8e6789
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-06 07:49:28 +0530

    seperate variables

--- 27-shaders/src/shaders/vertex.glsl
+++ 27-shaders/src/shaders/vertex.glsl
@@ -4,5 +4,8 @@ uniform mat4 modelMatrix;
 attribute vec3 position;
 
 void main(){
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectionPosition = projectionMatrix * viewPosition;
+    gl_Position = projectionPosition;
 }
