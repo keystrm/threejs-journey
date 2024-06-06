ae6d31d405d33bafa99ae45476d5900d5a207cd6
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-06 08:10:27 +0530

    change colors

--- 27-shaders/src/shaders/vertex.glsl
+++ 27-shaders/src/shaders/vertex.glsl
@@ -4,6 +4,7 @@ uniform mat4 modelMatrix;
 attribute vec3 position;
 
 attribute float aRandom;
+varying float vRandom;
 
 void main(){
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
@@ -12,4 +13,6 @@ void main(){
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
     gl_Position = projectionPosition;
+
+    vRandom = aRandom;
 }
