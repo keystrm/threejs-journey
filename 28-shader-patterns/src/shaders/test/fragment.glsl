e638ca547fb7befdad8e2438121de53b1644691f
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-10 00:28:22 +0530

    pattern 17

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -2,6 +2,6 @@ varying vec2 vUv;
 
 void main()
 {
-    float strength = abs(vUv.x - 0.5);
+    float strength = min(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
