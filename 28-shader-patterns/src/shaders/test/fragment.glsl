e29c80750a31f10bce41d456a0910d907a96e47d
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-10 00:56:01 +0530

    pattern 39

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -15,8 +15,8 @@ vec2 rotate(vec2 uv, float rotation, vec2 mid)
 void main()
 {
     vec2 wavedUv = vec2(
-        vUv.x + sin(vUv.y * 30.0) * 0.1,
-        vUv.y + sin(vUv.x * 30.0) * 0.1
+        vUv.x + sin(vUv.y * 100.0) * 0.1,
+        vUv.y + sin(vUv.x * 100.0) * 0.1
     );
     float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
 
