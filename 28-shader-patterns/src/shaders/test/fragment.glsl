aa5c8e53345daa717a19617586dfbe567e531a72
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-10 00:45:24 +0530

    pattern 30

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -7,6 +7,6 @@ float random(vec2 st)
 
 void main()
 {
-    float strength = 0.015 / (distance(vUv, vec2(0.5)));
+    float strength = 0.15 / (distance(vec2(vUv.x, (vUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
