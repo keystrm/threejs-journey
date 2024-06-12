dfa8866e9f197ce52eed5910607c8accd0635439
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-12 08:15:08 +0530

    change particles shape

--- 30-animated-galaxy/src/shaders/galaxy/fragment.glsl
+++ 30-animated-galaxy/src/shaders/galaxy/fragment.glsl
@@ -1,4 +1,8 @@
 void main()
 {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    float strength = distance(gl_PointCoord, vec2(0.5));
+    strength *= 2.0;
+    strength = 1.0 - strength;
+
+    gl_FragColor = vec4(vec3(strength), 1.0);
 }
