fad43101ac6eea5b418b5586a705a10f6895d8b2
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-19 23:40:37 +0530

    pass from child to parents

--- 54-first-react-application/src/App.jsx
+++ 54-first-react-application/src/App.jsx
@@ -3,15 +3,22 @@ import { useState } from 'react'
 
 export default function App({children}) {
     const [hasClicker, setHasClicker] = useState(true)
+    const [count,setCounter] = useState(0)
+
+    const increment = () => {
+        setCounter(count+1)
+    }
+
     return <>
             {children}
             <button onClick={() => setHasClicker(!hasClicker)}>Clicker</button>
             { hasClicker && <>
-                <Clicker keyName="countA" color={ `hsl(${ Math.random() * 360 }deg, 100%, 70%)` } />
-                <Clicker keyName="countB" color={ `hsl(${ Math.random() * 360 }deg, 100%, 70%)` } />
-                <Clicker keyName="countC" color={ `hsl(${ Math.random() * 360 }deg, 100%, 70%)` } />
+                <Clicker increment={increment} keyName="countA" color={ `hsl(${ Math.random() * 360 }deg, 100%, 70%)` } />
+                <Clicker increment={increment} keyName="countB" color={ `hsl(${ Math.random() * 360 }deg, 100%, 70%)` } />
+                <Clicker increment={increment} keyName="countC" color={ `hsl(${ Math.random() * 360 }deg, 100%, 70%)` } />
              </>
             }
+            <div>Total count : {count}</div>
         </>
 
 }
--- 54-first-react-application/src/Clicker.jsx
+++ 54-first-react-application/src/Clicker.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-export function Clicker({keyName,color}) {
+export function Clicker({keyName,color,increment}) {
     const [ count, setCount ] = useState(parseInt(localStorage.getItem(keyName) ?? 0))
     useEffect(() =>
     {
@@ -14,10 +14,15 @@ export function Clicker({keyName,color}) {
         localStorage.setItem(keyName, count)
     }, [ count ])
 
+    const buttonClick = () => {
+        setCount(count + 1)
+        increment()
+    }
+
     return (
         <div>
             <div style={{color:color}}>Clicks count: {count}</div>
-            <button onClick={() => setCount(count + 1)}>Click me</button>
+            <button onClick={buttonClick}>Click me</button>
         </div>
     )
 }
