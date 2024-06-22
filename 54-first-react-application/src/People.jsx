bf48f4d0a0c6d82d2e719473862f70a8c7cd4fc0
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-22 14:24:13 +0530

    fetch from apis

--- 54-first-react-application/src/People.jsx
+++ 54-first-react-application/src/People.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 export function People()
 {
@@ -11,6 +11,18 @@ export function People()
         {id: 6, name: 'Jonas' },
         {id: 7, name: 'Jamal' }
     ])
+
+    const getPeople = async () =>
+    {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const result = await response.json()
+        setPeople(result)
+    }
+    
+    useEffect(() =>
+    {
+        getPeople()
+    }, [])
 
     return <>
         <h2>People</h2>
