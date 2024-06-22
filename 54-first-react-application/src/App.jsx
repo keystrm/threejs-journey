19126e2174808947133146b535a25b1957e2ee4d
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-22 13:27:01 +0530

    useMemo to prevent whole page refresh

--- 54-first-react-application/src/App.jsx
+++ 54-first-react-application/src/App.jsx
@@ -1,5 +1,5 @@
 import { Clicker } from "./Clicker";
-import { useState } from 'react'
+import { useState,useMemo } from 'react'
 
 export default function App({clickersCount,children}) {
     const [hasClicker, setHasClicker] = useState(true)
@@ -8,6 +8,14 @@ export default function App({clickersCount,children}) {
     const increment = () => {
         setCounter(count+1)
     }
+		
+    const colors = useMemo(() =>
+    {
+        const colors = []
+        for(let i = 0; i < clickersCount; i++)
+            colors.push(`hsl(${ Math.random() * 360 }deg, 100%, 75%)`)
+        return colors
+    }, [clickersCount])
 
     return <>
             {children}
@@ -18,7 +26,7 @@ export default function App({clickersCount,children}) {
                         key = {index}
                         increment={ increment }
                         keyName={ `count${index}` }
-                        color={ `hsl(${ Math.random() * 360 }deg, 100%, 75%)` }
+                        color={colors[index]}
                     />
                 ) }
         </> }
