19fc450a69e4675b794083fe4d0301291c1a9769
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-23 23:25:03 +0530

    adding groups

--- 55-first-r3f-application/src/Experience.jsx
+++ 55-first-r3f-application/src/Experience.jsx
@@ -3,21 +3,24 @@ import { useRef } from 'react'
 export default function Experience()
 {
     const cubeRef = useRef()
+    const groupRef = useRef()
     useFrame((state, delta) =>
     {
-        console.log(delta)
         cubeRef.current.rotation.y += 0.01
+        groupRef.current.rotation.y += delta
     })
     return <>
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshBasicMaterial color="orange" />
-        </mesh>
+        <group ref={groupRef}>
+            <mesh position-x={ - 2 }>
+                <sphereGeometry />
+                <meshBasicMaterial color="orange" />
+            </mesh>
 
-        <mesh  ref={ cubeRef } rotation-y={ Math.PI * 0.25 } position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshBasicMaterial color="mediumpurple" />
-        </mesh>
+            <mesh  ref={ cubeRef } rotation-y={ Math.PI * 0.25 } position-x={ 2 } scale={ 1.5 }>
+                <boxGeometry />
+                <meshBasicMaterial color="mediumpurple" />
+            </mesh>
+        </group>
 
         <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
