f5abeb53b989e7e754ba1aeed9309f56df5023eb
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-24 23:40:11 +0530

    animating the camera

--- 55-first-r3f-application/src/index.jsx
+++ 55-first-r3f-application/src/index.jsx
@@ -7,10 +7,8 @@ const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <Canvas 
-        orthographic
         camera={ { 
             fov: 45, 
-            zoom: 100,
             near: 0.1, 
             far: 200, 
             position: [ 3, 2, 6 ] 
