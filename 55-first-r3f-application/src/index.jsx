5c16bc9a61f6c8d49d5070c7e4992fce58966d58
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-24 23:45:40 +0530

    tweaking values with three

--- 55-first-r3f-application/src/index.jsx
+++ 55-first-r3f-application/src/index.jsx
@@ -2,11 +2,17 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience'
+import * as THREE from 'three' 
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <Canvas 
+        gl={ {
+            antialias: true,
+            toneMapping: THREE.ACESFilmicToneMapping,
+            outputColorSpace: THREE.LinearSRGBColorSpace
+        } }
         camera={ { 
             fov: 45, 
             near: 0.1, 
