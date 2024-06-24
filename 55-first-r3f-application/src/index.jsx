905f25086ade153a96032a20575c47e9e7eb79c6
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-24 23:35:18 +0530

    orthographic camera

--- 55-first-r3f-application/src/index.jsx
+++ 55-first-r3f-application/src/index.jsx
@@ -7,8 +7,10 @@ const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <Canvas 
+        orthographic
         camera={ { 
             fov: 45, 
+            zoom: 100,
             near: 0.1, 
             far: 200, 
             position: [ 3, 2, 6 ] 
