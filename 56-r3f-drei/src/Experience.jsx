02881320b5a25f492a8ca422c6dee0a0617e7784
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-25 23:48:07 +0530

    pivots

--- 56-r3f-drei/src/Experience.jsx
+++ 56-r3f-drei/src/Experience.jsx
@@ -1,19 +1,28 @@
-import { OrbitControls, TransformControls } from '@react-three/drei'
+import { Html,PivotControls, TransformControls, OrbitControls } from '@react-three/drei'
 import { useRef } from 'react'
 export default function Experience()
 {
     const cube = useRef()
     return <>
-        <OrbitControls />
+        <OrbitControls makeDefault />
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
         <ambientLight intensity={ 1.5 } />
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+        <PivotControls
+            anchor={ [ 0, 0, 0 ] }
+            depthTest={ false }
+            lineWidth={ 4 }
+            axisColors={ [ '#9381ff', '#ff4d6d', '#7ae582' ] }
+            scale={ 100 }
+            fixed={ true }
+        >
+            <mesh position-x={ - 2 }>
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+            </mesh>
+        </PivotControls>
 
-        <TransformControls object={ cube }>
+        <TransformControls object={ cube } mode="rotate" >
             <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
                 <boxGeometry />
                 <meshStandardMaterial color="mediumpurple" />
