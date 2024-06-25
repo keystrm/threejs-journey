7bf2cff6b4a6c43e0a3b9e3c13af27fa62d55b8c
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-06-25 23:35:22 +0530

    add OrbitControls to cube

--- 56-r3f-drei/src/Experience.jsx
+++ 56-r3f-drei/src/Experience.jsx
@@ -1,6 +1,8 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, TransformControls } from '@react-three/drei'
+import { useRef } from 'react'
 export default function Experience()
 {
+    const cube = useRef()
     return <>
         <OrbitControls />
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
@@ -11,10 +13,12 @@ export default function Experience()
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+        <TransformControls object={ cube }>
+            <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
+                <boxGeometry />
+                <meshStandardMaterial color="mediumpurple" />
+            </mesh>
+        </TransformControls>
 
         <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
